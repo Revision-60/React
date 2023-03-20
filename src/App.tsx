7eb778ce9ca7bc2029dcc92f4d62bcf18b9f8a5d
Sandun Induranga <sandutharu40@gmail.com>
2023-03-20 19:54:48 +0530

    Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 }
---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,14 @@
+import { Component, Fragment } from "react";
+
+export default class Content extends Component {
+  render() {
+    return (
+      // Fragment
+      <>
+        <div className="flex justify-center items-center h-screen">
+          <h1>Content</h1>
+        </div>
+      </>
+    );
+  }
+}
