a24939e70994e532d8e8b464024d1d58e00efdad
Sandun Induranga <sandutharu40@gmail.com>
2023-03-24 17:36:46 +0530

    Counter Component Arrow Functions

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -17,10 +17,32 @@ export default class Counter extends Component<CounterProps, CounterState> {
     };
   }
 
+  increaseCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decreaseCount = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
       <div className="h-80 bg-slate-900 px-40 text-white border flex justify-center items-center flex-col gap-6 rounded-lg">
         <h1>{this.state.count}</h1>
+        <section className="flex gap-6">
+          <button
+            className="text-4xl border-2 w-12 h-12 rounded-lg"
+            onClick={this.decreaseCount}
+          >
+            -
+          </button>
+          <button
+            className="text-4xl border-2 w-12 h-12 rounded-lg"
+            onClick={this.increaseCount}
+          >
+            +
+          </button>
+        </section>
       </div>
     );
   }
