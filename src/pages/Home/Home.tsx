b212bca8e7fd480215c22fc25c873d32e30aae40
Sandun Induranga <sandutharu40@gmail.com>
2023-03-30 22:02:41 +0530

    Student

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,21 @@
-import React, { Component } from "react";
+import { Paper } from "@mui/material";
+import { Component } from "react";
 
 export default class Home extends Component {
   render() {
-    return <h1>Home</h1>;
+    return (
+      <div>
+        <Paper elevation={3} className="p-10 text-center">
+          <h3 className="text-3xl font-semibold">S001</h3>
+          <h2 className="text-2xl">Gihan</h2>
+          <p className="text-2xl">Matara</p>
+          <div className="flex gap-4 mt-2">
+            <span className="p-3 border rounded">Maths</span>
+            <span className="p-3 border rounded">Science</span>
+            <span className="p-3 border rounded">English</span>
+          </div>
+        </Paper>
+      </div>
+    );
   }
 }
