4e2caf8e31e730888be63fb3be64fb4803285ad7
Sandun Induranga <sandutharu40@gmail.com>
2023-03-30 23:30:52 +0530

    Added Active Styles to NavLinks

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,8 @@ import userImage from "../../assets/user.png";
 
 export default class Header extends Component {
   render() {
+    const navStyles = "font-bold border-b-2 text-blue-200";
+
     return (
       <section className="bg-blue-900 w-full h-20 p-2.5 text-white fixed">
         <div className="h-full flex">
@@ -11,13 +13,31 @@ export default class Header extends Component {
             <h1 className="pl-6 text-3xl">LOGO</h1>
           </section>
           <section className="h-full w-1/2 flex items-center gap-6 justify-end text-lg">
-            <NavLink to={"/"}>Home</NavLink>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => (isActive ? navStyles : "")}
+            >
+              Home
+            </NavLink>
 
-            <NavLink to={"/about"}>About</NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) => (isActive ? navStyles : "")}
+            >
+              About
+            </NavLink>
 
-            <NavLink to={"/contact"}>Contact Us</NavLink>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) => (isActive ? navStyles : "")}
+            >
+              Contact Us
+            </NavLink>
 
-            <NavLink to={"/"}>
+            <NavLink
+              to={"/abc"}
+              className={({ isActive }) => (isActive ? navStyles : "")}
+            >
               <img src={userImage} className="w-10 h-10" alt="" />
             </NavLink>
           </section>
